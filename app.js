c680af1190e43d8ef2a12a05d6721642559f535e
conradolega <conradolega@gmail.com>
2015-06-25 19:06:09 +0800

    Create a Data service

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 angular.module('angularTutorial', [])
-    .controller('TutorialCtrl', function TutorialCtrl($scope, $timeout) {
-        $scope.data = {timer: 0};
-        var updateTimer = function() {
-            $scope.data.timer++;
-            $timeout(updateTimer, 1000);
-        };
-        updateTimer();
+    .factory('Data', function() {
+        return [
+            {name: 'CASAA'},
+            {name: 'Shopping Center'},
+            {name: 'Lutong Bahay'},
+            {name: 'Katag'},
+            {name: 'Iskomai'},
+            {name: 'Tinapay'},
+            {name: 'Chocolate Kiss'},
+            {name: 'ROC'},
+            {name: 'Beach House'},
+            {name: 'Snack Shack'}
+        ];
     })
-    .controller('NestedCtrl', function NestedCtrl($scope) {
-        $scope.data.name2 = 'Name2';
-    });
+    .controller('TutorialCtrl', function TutorialCtrl($scope, $timeout, Data) {
+        $scope.foods = Data;
+    })
+
