4cf6bf48e1547b20a850717088daa76c63be8c71
conradolega <conradolega@gmail.com>
2015-06-25 19:59:01 +0800

    Add a custom filter

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 angular.module('angularTutorial', [])
     .factory('Data', function() {
         return [
-            {name: 'CASAA'},
-            {name: 'Shopping Center'},
-            {name: 'Lutong Bahay'},
-            {name: 'Katag'},
-            {name: 'Iskomai'},
-            {name: 'Tinapay'},
-            {name: 'Chocolate Kiss'},
-            {name: 'ROC'},
-            {name: 'Beach House'},
-            {name: 'Snack Shack'}
+            {name: 'CASAA', rating: 4},
+            {name: 'Shopping Center', rating: 4},
+            {name: 'Lutong Bahay', rating: 3},
+            {name: 'Katag', rating: 3},
+            {name: 'Iskomai', rating: 3},
+            {name: 'Tinapay', rating: 4},
+            {name: 'Chocolate Kiss', rating: -1},
+            {name: 'ROC', rating: -1},
+            {name: 'Beach House', rating: 4},
+            {name: 'Snack Shack', rating: 4}
         ];
     })
+    .filter('showStars', function() {
+        return function(rating) {
+            return rating > 0 ? new Array(rating + 1).join('*') : 'N/A';
+        };
+     })
     .controller('TutorialCtrl', function TutorialCtrl($scope, $timeout, Data) {
         $scope.foods = Data;
     })
