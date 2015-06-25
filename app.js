ab3dbf592d7db4f53fc01412bd492908ef8e6943
conradolega <conradolega@gmail.com>
2015-06-25 10:02:06 +0800

    Add clock

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 angular.module('angularTutorial', [])
-    .controller('TutorialCtrl', function TutorialCtrl() {
+    .controller('TutorialCtrl', function TutorialCtrl($scope, $timeout) {
+        var updateClock = function() {
+            $scope.clock = new Date();
+            $timeout(function() {
+                updateClock();
+            }, 1000);
+        };
+        updateClock();
     });
